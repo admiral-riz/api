8efee587b6845d893a7b7a7c54a3614349eafc60
Rizqi-Abdul <lexyp0359@gmail.com>
2025-05-27 14:27:55 +0700

    update koneksi

--- koneksimysql.php
+++ koneksimysql.php
@@ -1,10 +1,15 @@
 <?php
 
-define('host', 'localhost');
-define('user', 'root');
+// define('host', 'localhost');
 // define('user', 'root');
-define('password', '');
-define('database', 'uts-uasmobile2');
+// // define('user', 'root');
+// define('password', '');
+// define('database', 'uts-uasmobile2');
+
+define('host', 'localhost');
+define('user', 'androidrisky');
+define('password', 'Risky123');
+define('database', 'androidrisky');
 
 $conn = mysqli_connect(host, user, password, database);
 if (!$conn) {
