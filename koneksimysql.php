0bc1d86d46e33132d2670f607b1dbde784c19102
Rizqi-Abdul <lexyp0359@gmail.com>
2025-06-20 17:52:58 +0700

    update change koneksimysql.php

--- koneksimysql.php
+++ koneksimysql.php
@@ -1,14 +1,14 @@
 <?php
 
-define('host', 'localhost');
-define('user', 'root');
-define('password', '');
-define('database', 'uts-uasmobile2');
-
 // define('host', 'localhost');
-// define('user', 'androidrisky');
-// define('password', 'Risky123');
-// define('database', 'androidrisky');
+// define('user', 'root');
+// define('password', '');
+// define('database', 'uts-uasmobile2');
+
+define('host', 'localhost');
+define('user', 'androidrisky');
+define('password', 'Risky123');
+define('database', 'androidrisky');
 
 $conn = mysqli_connect(host, user, password, database);
 if (!$conn) {
