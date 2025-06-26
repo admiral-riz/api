501e3ef194cf1f8cba2b3b134f3037caac2c7fe7
umrmaulana <maulanau147@gmail.com>
2025-06-26 10:32:36 +0700

    fix: update sorting order in product query to hargajual ascending

--- get_produk.php
+++ get_produk.php
@@ -9,7 +9,7 @@ if ($conn->connect_error) {
     exit();
 }
 
-$sql = "SELECT kode, merk, kategori, hargajual, stok, foto, deskripsi, view_count FROM tbl_product";
+$sql = "SELECT kode, merk, kategori, hargajual, stok, foto, deskripsi, view_count FROM tbl_product ORDER BY hargajual asc";
 $result = $conn->query($sql);
 
 $products = array();
